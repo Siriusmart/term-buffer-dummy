0849a3d2aa80d74c32658ee81407efabe44621f9
Siriusmart <sirius@siri.ws>
2026-03-05 15:51:51 +0000

    cell style definition

--- app/src/main/java/ws/siri/termbuffer/CellStyle.java
+++ app/src/main/java/ws/siri/termbuffer/CellStyle.java
@@ -3,5 +3,39 @@ package ws.siri.termbuffer;
 /**
  * CellStyle
  */
-public class CellStyle {
+public record CellStyle(Color fg, Color bg, boolean bold, boolean italic, boolean underline) {
+    /**
+     * create copy of cell style with specified fg colour
+     */
+    public CellStyle withFg(Color fg) {
+        return new CellStyle(fg, bg, bold, italic, underline);
+    }
+
+    /**
+     * create copy of cell style with specified bg colour
+     */
+    public CellStyle withBg(Color bg) {
+        return new CellStyle(fg, bg, bold, italic, underline);
+    }
+
+    /**
+     * create copy of cell style with specified bold style
+     */
+    public CellStyle withBold(boolean bold) {
+        return new CellStyle(fg, bg, bold, italic, underline);
+    }
+
+    /**
+     * create copy of cell style with specified italic style
+     */
+    public CellStyle withItalic(boolean italic) {
+        return new CellStyle(fg, bg, bold, italic, underline);
+    }
+
+    /**
+     * create copy of cell style with specified italic style
+     */
+    public CellStyle withUnderline(boolean underline) {
+        return new CellStyle(fg, bg, bold, italic, underline);
+    }
 }
