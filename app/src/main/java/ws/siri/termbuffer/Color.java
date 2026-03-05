9eb28a68640c266d186665f64074ddcd2f821e21
Siriusmart <sirius@siri.ws>
2026-03-05 15:26:45 +0000

    the 16 ansi term colours

---
+++ app/src/main/java/ws/siri/termbuffer/Color.java
@@ -0,0 +1,24 @@
+package ws.siri.termbuffer;
+
+/**
+ * The 16 ANSI terminal colours and default (no colour).
+ */
+public enum Color {
+    DEFAULT,
+    BLACK,
+    DARK_RED,
+    DARK_GREEN,
+    DARK_YELLOW,
+    DARK_BLUE,
+    DARK_MAGENTA,
+    DARK_CYAN,
+    DARK_WHITE,
+    BRIGHT_BLACK,
+    BRIGHT_RED,
+    BRIGHT_GREEN,
+    BRIGHT_YELLOW,
+    BRIGHT_BLUE,
+    BRIGHT_MAGENTA,
+    BRIGHT_CYAN,
+    WHITE
+}
