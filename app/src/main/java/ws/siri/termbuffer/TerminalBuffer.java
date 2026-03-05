64f2146a62c5f1f46d177736fe88431773668cb1
Siriusmart <sirius@siri.ws>
2026-03-05 15:52:29 +0000

    termbuffer implements itermbuffer

--- app/src/main/java/ws/siri/termbuffer/TerminalBuffer.java
+++ app/src/main/java/ws/siri/termbuffer/TerminalBuffer.java
@@ -1,10 +1,130 @@
 package ws.siri.termbuffer;
 
-public class TerminalBuffer {
+public class TerminalBuffer implements ITerminalBuffer {
     /**
      * Create new terminal buffer with initial settings
      */
     public TerminalBuffer(int width, int height, int maxScrollback) {
         throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public void setCursorFg(Color color) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setCursorFg'");
+    }
+
+    @Override
+    public void setCursorBg(Color color) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setCursorBg'");
+    }
+
+    @Override
+    public void setBold(boolean b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setBold'");
+    }
+
+    @Override
+    public void setItalic(boolean b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setItalic'");
+    }
+
+    @Override
+    public void setUnderline(boolean b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setUnderline'");
+    }
+
+    @Override
+    public void setCursorX(int x) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setCursorX'");
+    }
+
+    @Override
+    public void setCursorY(int y) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setCursorY'");
+    }
+
+    @Override
+    public void moveCursorUp(int n) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveCursorUp'");
+    }
+
+    @Override
+    public void moveCursorDown(int n) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveCursorDown'");
+    }
+
+    @Override
+    public void moveCursorLeft(int n) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveCursorLeft'");
+    }
+
+    @Override
+    public void moveCursorRight(int n) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveCursorRight'");
+    }
+
+    @Override
+    public void replaceChar(char c) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'replaceChar'");
+    }
+
+    @Override
+    public void insertChar(char c) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insertChar'");
+    }
+
+    @Override
+    public void newEmptyLine() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'newEmptyLine'");
+    }
+
+    @Override
+    public void newLine(char c) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'newLine'");
+    }
+
+    @Override
+    public char charAt(Vec2I xy) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'charAt'");
+    }
+
+    @Override
+    public CellStyle styleAt(Vec2I xy) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'styleAt'");
+    }
+
+    @Override
+    public String getLineString(int y) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getLineString'");
+    }
+
+    @Override
+    public String getScreenString() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getScreenString'");
+    }
+
+    @Override
+    public String getAllString() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllString'");
     }
 }
